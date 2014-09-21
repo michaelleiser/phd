7c242159f41917450e7a721246686b39b8ca779e
tino <koblt1@bfh.ch>
2014-09-21 22:07:13 +0200

    update Patient form

--- phd/src/main/java/org/phd/test/Patient.java
+++ phd/src/main/java/org/phd/test/Patient.java
@@ -5,7 +5,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -16,8 +18,15 @@ import javax.faces.bean.SessionScoped;
 public class Patient implements Serializable {
 
 	private int id = 0;
-	private String firstname = "Dummy First name";
-	private String lastname = "Dummy Last name";
+	private String sex = "";
+	private String firstname = "";
+	private String lastname = "";
+	private String street = "";
+	private int nr = 1;
+	private String city = "";
+	private String zip = "";
+	private int telnumber = 0;
+	private String birth;
 
 	public Patient() {
 
@@ -43,6 +52,53 @@ public class Patient implements Serializable {
 		this.lastname = lastname;
 	}
 
+	public String addNewPatient(){
+		String stm1 = "SELECT * FROM testdb.patient WHERE firstname='"+firstname+"' AND lastname='"+lastname+"' AND birthday='"+ birth + "';";
+		String stm2 = "INSERT INTO testdb.patient (firstname, lastname, birthday, street, nr, city, zip, telnumber) VALUES('"+firstname+"','"+lastname+"','"+birth+"','"+street+"','"+nr+"','"+city+"','"+zip+"','"+telnumber+"');";
+		Connection con = MyConnection.getConnection();
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+			pst = con.prepareStatement(stm1);
+			pst.execute();
+			rs = pst.getResultSet();
+			if(rs.next()) {
+				return "home";
+			}
+			pst.close();
+			pst = con.prepareStatement(stm2);
+			pst.execute();
+			pst.close();
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (pst != null) {
+				try {
+					pst.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+		return "home";
+	}
+	
 	public static List<Patient> getPatients() {
 		List<Patient> patients = new ArrayList<Patient>();
 		Connection con = MyConnection.getConnection();
@@ -89,6 +145,62 @@ public class Patient implements Serializable {
 			}
 		}
 		return patients;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getNr() {
+		return nr;
+	}
+
+	public void setNr(int nr) {
+		this.nr = nr;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public int getTelnumber() {
+		return telnumber;
+	}
+
+	public void setTelnumber(int telnumber) {
+		this.telnumber = telnumber;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;	
 	}
 
 }
